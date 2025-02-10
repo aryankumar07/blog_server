f410417bb2942043475c3f900a273b16a72c3fc8
aryankumar07 <ak2839426@gmail.com>
2025-02-10 19:57:17 +0530

    feat(UI): added filters

--- routes/post.route.js
+++ routes/post.route.js
@@ -3,6 +3,7 @@ const router = express.Router()
 import { getPosts, getSinglePost, createPost, deletePost, uploadAuth, featurePost, getFeaturedPost } from '../controller/post.controller.js'
 import Auth from '../middlewares/auth.js'
 import { requireAuth } from '@clerk/express'
+import increaseVisit from '../middlewares/increaseVisit.js'
 
 
 
@@ -11,7 +12,7 @@ router.get('/upload-auth', uploadAuth)
 // get routes
 router.get('/', getPosts)
 router.get('/featuredPost', getFeaturedPost)
-router.get('/:slug', getSinglePost)
+router.get('/:slug', increaseVisit, getSinglePost)
 
 
 
